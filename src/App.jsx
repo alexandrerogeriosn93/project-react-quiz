ad69812c5c80b1a99bcd0a7ebea58791526ead88
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-23 00:11:03 -0300

    feat: create component PickCategory

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useContext, useEffect } from "react";
 import { QuizContext } from "./context/quiz";
 
 import GameOver from "./components/GameOver";
+import PickCategory from "./components/PickCategory";
 import Question from "./components/Question";
 import Welcome from "./components/Welcome";
 
@@ -18,6 +19,7 @@ function App() {
     <div className="App">
       <h1>Quiz App</h1>
       {quizState.gameStage === "Start" && <Welcome />}
+      {quizState.gameStage === "Category" && <PickCategory />}
       {quizState.gameStage === "Playing" && <Question />}
       {quizState.gameStage === "End" && <GameOver />}
     </div>
