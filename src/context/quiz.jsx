53d187ebb8b4722bf1f7aff3ff9a6a47df463720
Alexandre Rogério <alexandrerogeriosn93@gmail.com>
2024-03-22 23:36:12 -0300

    feat: add function to process answers

--- src/context/quiz.jsx
+++ src/context/quiz.jsx
@@ -9,11 +9,10 @@ const initialState = {
   questions,
   currentQuestion: 0,
   score: 0,
+  answerSelected: false,
 };
 
 const quizReducer = (state, action) => {
-  console.log(state, action);
-
   switch (action.type) {
     case "CHANGE_STATE":
       return { ...state, gameStage: STAGES[1] };
@@ -34,6 +33,20 @@ const quizReducer = (state, action) => {
       };
     case "NEW_GAME":
       return initialState;
+    case "CHECK_ANSWER":
+      if (state.answerSelected) return state;
+
+      const answer = action.payload.answer;
+      const option = action.payload.option;
+      let correctAnswer = 0;
+
+      if (answer === option) correctAnswer = 1;
+
+      return {
+        ...state,
+        score: state.score + correctAnswer,
+        answerSelected: option,
+      };
     default:
       return state;
   }
